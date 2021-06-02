4a20033a6da575d1274bc34d3d6c8e6ed9562437
Mdakhib <mohamedakhib@gmail.com>
2021-06-02 11:50:14 +0530

    buttons file created and added to the signup button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import '../App.css'
 import { Link } from "react-router-dom";
 import { MdFingerprint } from "react-icons/md";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "./Button";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click);
+  const [button, setButton] = useState(true);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false)
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
 
+    window.addEventListener('resize',showButton)
+    
   return (
     <>
       <div className="navbar">
@@ -19,6 +32,36 @@ const Navbar = () => {
           <div className="menu-icon" onClick={handleClick}>
             {click ? <FaTimes /> : <FaBars />}
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link className="nav-links" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-links" to="/services">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-links" to="/products">
+                Products
+              </Link>
+            </li>
+            <li className="nav-btn">
+              {button ? (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline">SIGN UP</Button>
+                </Link>
+              ) : (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                    SIGN UP
+                  </Button>
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </>
