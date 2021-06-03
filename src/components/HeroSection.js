021d01016baba1def8b75c2e4be963c289d3b5b3
Mdakhib <mohamedakhib@gmail.com>
2021-06-03 11:37:08 +0530

    by using props children created lightbg and darkbg using ternary operator we can change into light and dark by using that and styling also

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "./HeroSection.css";
+import src from "*.avif";
 
 function HeroSection({
   lightBg,
@@ -10,7 +11,7 @@ function HeroSection({
   lightTextDesc,
   headLine,
   description,
-  buttonLine,
+  buttonLable,
   img,
   alt,
   imgStart,
@@ -21,8 +22,26 @@ function HeroSection({
         className={lightBg ? "home__hero-section" : "home__hero-section darkBg"}
       >
         <div className="container">
-          <div className="row home__hero-row">
-            
+          <div
+            className="row home__hero-row"
+            style={{
+              display: "flex",
+              flexDirection: imgStart === "start" ? "row-reverse" : "row",
+            }}
+          >
+            <div className="home__hero-text-wrapper">
+              <div className="top-line">{topLine} </div>
+              <h1 className={lightText ? 'heading' : 'heading dark'}>{headLine} </h1>
+              <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>{description} </p>
+              <Link to='/sign-up'>
+                <Button buttonSize='btn--wide' buttonColor='blue'>{buttonLable} </Button>
+              </Link>
+            </div>
+          </div>
+          <div className="col">
+            <div className="home__hero-img-wrapper">
+              <img src={img} alt={alt} className='home__hero-img' />
+            </div>
           </div>
         </div>
       </div>
